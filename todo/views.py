af81ad00f47454c2abea5b5cdbc2ad2e31d7ff82
Sancler Zanella <sanclerzjj@gmail.com>
2021-08-30 13:36:56 +0000

    Remove HTML form and add form by using django ModelForm functionality

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import (render, redirect)
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
@@ -13,11 +14,15 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == 'POST':
-        name = request.POST.get('item_name')
-        done = 'done' in request.POST
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
 
-        Item.objects.create(name=name, done=done)
+    form = ItemForm()
 
-        return redirect('get_todo_list')
+    context = {
+        'form': form
+    }
 
-    return render(request, 'todo/add_item.html')
+    return render(request, 'todo/add_item.html', context)
